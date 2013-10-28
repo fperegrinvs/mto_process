e791dc650d0d92ec283f7be72f28c667fe0a43b3
Leonardo Stern <leonardo.stern@gmail.com>
2013-10-28 21:37:37 -0200

    Integrando file manager ao TFS. Criando pasta quando necessário

--- Extensions/tfs.js
+++ Extensions/tfs.js
@@ -4,12 +4,20 @@ $(function(){
 	ext_plugin.base_path = window.location.protocol + '//' + window.location.hostname + '/';
 	ext_plugin.project_name = $('.project-name').html();
 	var element = $('.hub-groups');
+	element.append('<li id="files-link"><a href="javascript:files();">Files</a></li>');
 	element.append('<li id="wiki-link"><a href="javascript:wiki();">Wiki</a></li>');
 });
 
 function wiki() {
 	$('.hub-groups>.selected').removeClass('selected');
 	$('#wiki-link').addClass('selected');
-	$('.hubs-section>.hubs').html('<li class="selected">Wiki</li>');
+	$('.hubs-section>.hubs').html('<li class="selected"><a href="#">Wiki</a></li>');
 	$(".hub-content").html('<iframe width="100%" height="98%" src="'  + ext_plugin.base_path + 'wiki/' + ext_plugin.project_name + '"></iframe>');  
+}
+
+function files() {
+	$('.hub-groups>.selected').removeClass('selected');
+	$('#files-link').addClass('selected');
+	$('.hubs-section>.hubs').html('<li class="selected"><a href="#">Files</a></li>');
+	$(".hub-content").html('<iframe width="100%" height="98%" src="'  + ext_plugin.base_path + 'file?p=' + ext_plugin.project_name + '"></iframe>');  
 }
--- File/Default.aspx.cs
+++ File/Default.aspx.cs
@@ -3,6 +3,7 @@
 namespace FileManager
 {
     using System.Configuration;
+    using System.IO;
     using System.Web;
 
     using IZ.WebFileManager;
@@ -21,6 +22,14 @@ namespace FileManager
 
             string project = HttpContext.Current.Request.QueryString["p"];
             var basePath = ConfigurationManager.AppSettings["basePath"];
+            var root = basePath + "/" + project;
+
+            var folder = Server.MapPath(root);
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
             FileManager1.RootDirectories.Add(new RootDirectory { DirectoryPath = basePath + "/" + project, Text = project, });
         }
     }
